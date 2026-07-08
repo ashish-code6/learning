cabda800589d03f92dd25680e68c32daecf68474
ashish-code6 <ashishk810222@gmail.com>
2026-07-09 00:14:55 +0530

    add number-Datatypes

---
+++ TypeScript/number.js
@@ -0,0 +1,19 @@
+"use strict";
+var indiaScore = 201;
+var ausScore = 350;
+console.log(indiaScore);
+console.log(ausScore);
+// calculate infinty
+var marks = 10 / 0;
+console.log(marks);
+// nan value :
+var kuchahi = 0 / 0;
+console.log(kuchahi);
+// convert tring to number
+var food_price = "70";
+console.log(Number(food_price));
+var a = 0.1;
+var b = 0.2;
+console.log((a + b).toFixed(2));
+// o/p: 0.30000000000000004  
+// Computers store number values in binary (base 2), not decimal (base 10).
---
+++ TypeScript/number.ts
@@ -0,0 +1,32 @@
+var indiaScore:number=201
+var ausScore:number = 350
+
+console.log(indiaScore)
+console.log(ausScore)
+
+// calculate infinty
+
+var marks = 10/0
+
+console.log(marks)
+
+// nan value :
+
+var kuchahi = 0/0
+
+console.log(kuchahi)
+
+// convert tring to number
+
+var food_price = "70"
+
+console.log(Number(food_price))
+
+var a:number= 0.1
+var b:number= 0.2
+
+console.log((a+b).toFixed(2))
+// o/p: 0.30000000000000004  
+
+// Computers store number values in binary (base 2), not decimal (base 10).
+
